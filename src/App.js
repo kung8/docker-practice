d48dcd655a3fc5c1ba9777a972c70d74ad171969
Kevin Ung <ung.kevin78@gmail.com>
2021-01-09 22:16:17 -0700

    update the text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! <code>src/App.js</code> and save to reload.
+          Hi ya! This is some new text!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi ya! This is some new text!
+          Learn React
         </a>
       </header>
     </div>
